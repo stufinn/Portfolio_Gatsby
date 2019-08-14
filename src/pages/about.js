63d1f3f2cc57385a9a9f96eca30ee670f027ae9d
stufinn <30563561+finst@users.noreply.github.com>
2019-08-14 17:22:26 -0500

    Update about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,19 +8,8 @@ export default () => (
       <meta charSet="utf-8" /> <title>About</title>{" "}
     </Helmet>
     <div>
-      <h2>About Me</h2>
-      <p>
-        Bacon ipsum dolor amet short loin shank cow tri-tip, pastrami alcatra
-        ground round drumstick bacon swine landjaeger kielbasa. Frankfurter jowl
-        porchetta cow shoulder tri-tip short ribs andouille alcatra buffalo
-        pancetta kevin.
-      </p>
-      <p>
-        Prosciutto bresaola pork belly shankle, turducken venison swine buffalo
-        biltong frankfurter ground round. Turducken ball tip leberkas shank cow
-        frankfurter pancetta ribeye tongue sausage picanha alcatra salami
-        hamburger ground round.
-      </p>
+      <h2>About Stu</h2>
+      <p>This website is under development</p>
     </div>
   </div>
 )
