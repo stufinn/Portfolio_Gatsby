d189b400e7143814e6a476d90b90a6b0f9d820e2
stufinn <30563561+finst@users.noreply.github.com>
2019-08-14 16:30:28 -0500

    Remove HomeContent component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
-
-import HomeContent from "../components/homeContent"
+// import HomeContent from "../components/homeContent"
 
 // const sayHello = () =>
 
@@ -13,8 +12,23 @@ const Index = () => {
         <meta charSet="utf-8" /> <title>Stu Finn</title>{" "}
       </Helmet>
 
-      <div>
-        <HomeContent />
+      <div />
+      {/* 
+      <p className="statement">
+        <span>Custom websites and web-based applications</span>
+      </p> */}
+      {/* <HomeContent /> */}
+      <div className="homeContent">
+        {/* <h3>I am</h3> */}
+        <p className="statement">
+          I am a <span>developer</span> who specializes in cutting-edge web
+          technologies.
+        </p>
+        {/* <h3>I create</h3> */}
+        <p className="statement">
+          I help clients like you to, <span>design & build</span> custom
+          websites and web applications.
+        </p>
       </div>
     </div>
   )
