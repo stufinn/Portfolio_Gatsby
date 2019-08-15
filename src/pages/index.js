d407c64ddc5b3681e93857d1f3c5aab5b8a05aaa
stufinn <30563561+finst@users.noreply.github.com>
2019-08-15 14:27:20 -0500

    Add link buttons and change text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,38 @@
 import React from "react"
 import Helmet from "react-helmet"
+import LinkButton from "../components/linkButton"
 // import HomeContent from "../components/homeContent"
 
 // const sayHello = () =>
 
 const Index = () => {
   return (
-    <div>
+    <div className="homeContent">
       <Helmet>
         {" "}
         <meta charSet="utf-8" /> <title>Stu Finn</title>{" "}
       </Helmet>
 
-      <div />
       {/* 
       <p className="statement">
         <span>Custom websites and web-based applications</span>
       </p> */}
       {/* <HomeContent /> */}
-      <div className="homeContent">
-        {/* <h3>I am</h3> */}
-        <p className="statement">
-          I am a <span>developer</span> who specializes in cutting-edge web
-          technologies.
-        </p>
-        {/* <h3>I create</h3> */}
-        <p className="statement">
-          I help clients like you to <span>design & build</span> custom websites
-          and web applications.
-        </p>
-      </div>
+
+      {/* <h3>I am</h3> */}
+      <p className="statement">
+        Working with <span>clients like you</span> to build modern,{" "}
+        <span>custom</span> websites and web applications.
+      </p>
+
+      <LinkButton buttonText="Samples of My Work" linkToPage="portfolio" />
+      <LinkButton buttonText="Get In Touch" linkToPage="portfolio" />
+
+      {/* <h3>I create</h3> */}
+      {/* <p className="statement">
+        I help clients like you to <span>design & build</span> custom websites
+        and web applications.
+      </p> */}
     </div>
   )
 }
