1eb841234486bd55f6e1b2d4dea9d79b4fb2141b
stufinn <30563561+finst@users.noreply.github.com>
2019-08-21 23:12:28 -0500

    Center about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Helmet from "react-helmet"
 
 export default () => (
-  <div>
+  <div className="aboutContent">
     <Helmet>
       {" "}
       <meta charSet="utf-8" /> <title>About</title>{" "}
