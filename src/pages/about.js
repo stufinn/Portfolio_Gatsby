368243689c71fb14a8a9cc9b9d5d77ac0b0566fb
stufinn <30563561+finst@users.noreply.github.com>
2019-08-27 18:07:04 -0500

    Add styles to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,47 +1,70 @@
 import React from "react"
 import Helmet from "react-helmet"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-export default () => (
+export default ({ data }) => (
   <div className="aboutContent">
     <Helmet>
       {" "}
       <meta charSet="utf-8" /> <title>About</title>{" "}
     </Helmet>
-    <div>
-      <h2 className="projectsTitle">About Stu</h2>
-      <p style={{ paddingTop: 50 }} className="aboutPage__text">
-        Hi! <span role="img">👋</span> I am a{" "}
-        <span className="aboutPage--emph">software developer</span> based out of
-        a beautiful little town called Sioux Lookout in Northwestern Ontario.
-      </p>
-      <p className="aboutPage__text">
-        I have a passion for creating{" "}
-        <span className="aboutPage--emph">interesting</span>,{" "}
-        <span className="aboutPage--emph">helpful</span>, and hopefully even{" "}
-        <span className="aboutPage--emph">beautiful</span> things with{" "}
-        <span className="aboutPage--emph">code</span>. I’ve got a background in
-        web development (surprise, surprise), biological sciences (Honours
-        Bsc.), international development (minor), and environmental studies
-        (Masters). I am also a certified professional project manager (PMP),
-        when my credential isn’t lapsed that is (<span role="img">😬</span>… I’m
-        on it!).
-      </p>
-      <p className="aboutPage__text">
-        The websites that I create are often inspired by{" "}
-        <span className="aboutPage--emph">nature</span> and{" "}
-        <span className="aboutPage--emph">minimalistic design</span>. I love
-        building products that look amazing and are easy to use. If you are
-        interested in my work, please check out{" "}
-        <Link to="/portfolio" className="aboutPage--emph">
-          my portfolio
-        </Link>{" "}
-        page or{" "}
-        <Link to="/contact" className="aboutPage--emph">
-          contact me
-        </Link>
-        .
-      </p>
-    </div>
+    <h2 className="projectsTitle">About Stu</h2>
+    <Img
+      className="aboutPage__pic"
+      fixed={data.stuImg.edges[0].node.childImageSharp.fixed}
+    />
+    <div className="aboutPage__text"></div>
+    <p>
+      Hi! <span role="img">👋</span> I am a{" "}
+      <span className="aboutPage--emph">software developer</span> based out of a
+      beautiful little town called Sioux Lookout, in Northwestern Ontario,
+      Canada.
+    </p>
+    <p>
+      I have a passion for creating{" "}
+      <span className="aboutPage--emph">interesting</span>,{" "}
+      <span className="aboutPage--emph">helpful</span>, and hopefully even{" "}
+      <span className="aboutPage--emph">beautiful</span> things with{" "}
+      <span className="aboutPage--emph">code</span>.
+    </p>{" "}
+    <p>
+      I’ve got a background in web development (surprise, surprise), an
+      undergrad in biological sciences and international development, a masters
+      degree in environmental studies. I am also a certified professional
+      project manager (PMP), when my credential isn’t lapsed that is (
+      <span role="img">😬</span>… I’m on it!).
+    </p>
+    <p>
+      The websites that I create are often inspired by{" "}
+      <span className="aboutPage--emph">nature</span> and{" "}
+      <span className="aboutPage--emph">minimalistic design</span>. I love
+      building products that look amazing and are easy to use. If you are
+      interested in my work, please check out{" "}
+      <Link to="/portfolio" className="aboutPage--emph">
+        my portfolio
+      </Link>{" "}
+      or{" "}
+      <Link to="/contact" className="aboutPage--emph">
+        contact me
+      </Link>
+      .
+    </p>
   </div>
 )
+
+export const pageQuery = graphql`
+  query aboutQuery {
+    stuImg: allFile(filter: { name: { eq: "Stu2019" } }) {
+      edges {
+        node {
+          childImageSharp {
+            fixed(height: 300, width: 300) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`
