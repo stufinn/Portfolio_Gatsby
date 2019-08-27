19544a904e77244677a96f5cbadd2f9797a9d7dd
stufinn <30563561+finst@users.noreply.github.com>
2019-08-27 17:23:43 -0500

    Add and format text to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Helmet from "react-helmet"
+import { Link } from "gatsby"
 
 export default () => (
   <div className="aboutContent">
@@ -9,8 +10,37 @@ export default () => (
     </Helmet>
     <div>
       <h2 className="projectsTitle">About Stu</h2>
-      <p style={{ paddingTop: 50 }}>
-        [ This page is currently under development. ]
+      <p style={{ paddingTop: 50 }} className="aboutPage__text">
+        Hi! <span role="img">👋</span> I am a{" "}
+        <span className="aboutPage--emph">software developer</span> based out of
+        a beautiful little town called Sioux Lookout in Northwestern Ontario.
+      </p>
+      <p className="aboutPage__text">
+        I have a passion for creating{" "}
+        <span className="aboutPage--emph">interesting</span>,{" "}
+        <span className="aboutPage--emph">helpful</span>, and hopefully even{" "}
+        <span className="aboutPage--emph">beautiful</span> things with{" "}
+        <span className="aboutPage--emph">code</span>. I’ve got a background in
+        web development (surprise, surprise), biological sciences (Honours
+        Bsc.), international development (minor), and environmental studies
+        (Masters). I am also a certified professional project manager (PMP),
+        when my credential isn’t lapsed that is (<span role="img">😬</span>… I’m
+        on it!).
+      </p>
+      <p className="aboutPage__text">
+        The websites that I create are often inspired by{" "}
+        <span className="aboutPage--emph">nature</span> and{" "}
+        <span className="aboutPage--emph">minimalistic design</span>. I love
+        building products that look amazing and are easy to use. If you are
+        interested in my work, please check out{" "}
+        <Link to="/portfolio" className="aboutPage--emph">
+          my portfolio
+        </Link>{" "}
+        page or{" "}
+        <Link to="/contact" className="aboutPage--emph">
+          contact me
+        </Link>
+        .
       </p>
     </div>
   </div>
