d55134bef4ecbb80655c7162b6e6b7e68c8f049b
stufinn <30563561+finst@users.noreply.github.com>
2019-08-27 22:00:40 -0500

    Add gradient border and improve text

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,25 +4,27 @@ import { Link, graphql } from "gatsby"
 import Img from "gatsby-image"
 
 export default ({ data }) => (
-  <div className="aboutContent">
+  <div className="aboutPage">
     <Helmet>
       {" "}
       <meta charSet="utf-8" /> <title>About</title>{" "}
     </Helmet>
-    <h2 className="projectsTitle">About Stu</h2>
-    <Img
-      className="aboutPage__pic"
-      fixed={data.stuImg.edges[0].node.childImageSharp.fixed}
-    />
+    {/* Profile photo with wrapper for gradient border */}
+    <h2 className="projectsTitle">About Me</h2>
+    <div className="aboutPage__borderGradientContainer">
+      <Img
+        className="aboutPage__pic"
+        fixed={data.stuImg.edges[0].node.childImageSharp.fixed}
+      />
+    </div>
     <div className="aboutPage__text"></div>
     <p>
       Hi!{" "}
       <span role="img" aria-label="hand-wave emoji">
         👋
       </span>{" "}
-      I am a <span className="aboutPage--emph">software developer</span> based
-      out of a beautiful little town called Sioux Lookout, in Northwestern
-      Ontario, Canada.
+      I'm a <span className="aboutPage--emph">software developer</span> based
+      out of Sioux Lookout, in Northwestern Ontario, Canada.
     </p>
     <p>
       I have a passion for creating{" "}
@@ -41,16 +43,18 @@ export default ({ data }) => (
       The websites that I create are often inspired by{" "}
       <span className="aboutPage--emph">nature</span> and{" "}
       <span className="aboutPage--emph">minimalistic design</span>. I love
-      building products that look amazing and are easy to use. If you are
-      interested in my work, please check out{" "}
+      building products that look amazing and are easy to use.
+    </p>
+    <p>
+      If you are interested in my work, please check out{" "}
       <Link to="/portfolio" className="aboutPage--emph">
         my portfolio
       </Link>{" "}
       or{" "}
       <Link to="/contact" className="aboutPage--emph">
-        contact me
+        get in touch
       </Link>
-      .
+      !
     </p>
   </div>
 )
