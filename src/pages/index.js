03c46d9f4098e354c5ab2b9e07bba311c2b05502
Stu Finn <stu@borealisweb.ca>
2022-01-13 15:52:10 -0600

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import { Link } from "gatsby"
 import React from "react"
 import Helmet from "react-helmet"
+import { Emph, H1 } from "../components/HelperComponents"
 import LinkButton from "../components/linkButton"
 // import HomeContent from "../components/homeContent"
 
@@ -9,12 +11,24 @@ const Highlight = ({ children }) => (
   <span className="text-main text-5xl">{children}</span>
 )
 
+const EmphIndex = ({ children }) => (
+  <Emph className=" text-4xl md:text-5xl">{children}</Emph>
+)
+
+const TextContainer = ({ children, title = "" }) => (
+  <div className=" text-center  mt-16 first:mt-0">
+    <H1 className="">{title}</H1>
+
+    <div className="mt-5">{children}</div>
+  </div>
+)
+
 const Index = () => {
   return (
-    <div className="text-center w-11/12 flex flex-col  items-center">
+    <div className="text-3xl md:text-4xl ">
       <Helmet>
         {" "}
-        <meta charSet="utf-8" /> <title>Stu Finn</title>{" "}
+        <meta charSet="utf-8" /> <title>Stu Finn, Web Developer</title>{" "}
       </Helmet>
 
       {/* 
@@ -24,14 +38,30 @@ const Index = () => {
       {/* <HomeContent /> */}
 
       {/* <h3>I am</h3> */}
-      <p className="statement mb-7 text-4xl max-w-md">
-        <Highlight>Blazing fast,</Highlight> modern and secure{" "}
-        <Highlight>custom-built websites</Highlight> and web applications.
-      </p>
 
-      <div className="index__links mt-24">
+      <TextContainer>
+        <p>
+          I am a <EmphIndex>front-end web developer</EmphIndex> based in
+          Northwestern Ontario, Canada.
+        </p>
+        <p className="mt-5">
+          I'm currently{" "}
+          <EmphIndex className="text-4xl">looking for part time work</EmphIndex>{" "}
+          as a software developer (remote).
+        </p>
+      </TextContainer>
+
+      <TextContainer title="My Work">
+        <p>
+          Check out some of my recent projects <Link to="/portfolio">here</Link>
+          .
+        </p>
+      </TextContainer>
+      <TextContainer title="Contact Stu"></TextContainer>
+
+      {/* <div className="index__links mt-24">
         <LinkButton buttonText="Get In Touch!" linkToPage="contact" />
-      </div>
+      </div> */}
 
       {/* <h3>I create</h3> */}
       {/* <p className="statement">
