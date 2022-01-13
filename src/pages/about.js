7038a83df8989ab20c44426136930f27bd2c752b
Stu Finn <stu@borealisweb.ca>
2022-01-13 15:13:22 -0600

    Chage to first person

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import {
   AiFillGithub,
   AiFillLinkedin,
   AiFillTwitterCircle,
+  AiOutlineArrowDown,
 } from "react-icons/ai"
 
 const Emph = ({ children }) => (
@@ -40,8 +41,8 @@ const About = ({ data }) => {
         <title>About Stu Finn</title>{" "}
       </Helmet>
       {/* Profile photo with wrapper for gradient border */}
-      <h1 className="projectsTitle">A bit of info</h1>
-      <div className="grid  justify-items-center md:block mt-10">
+      <h1 className="projectsTitle  text-5xl mt-10 md:mt-0">About Stu</h1>
+      <div className="grid  justify-items-center md:block mt-5 md:mt-10">
         <div className="aboutPage__borderGradientContainer aboutFloat grid bg-gradient-to-br from-main to-quaternary w-fit p-1 rounded-full my-6 mr-5  ">
           <Img
             className="aboutPage__pic rounded-full"
@@ -52,8 +53,8 @@ const About = ({ data }) => {
         </div>
         <div className="aboutPage__text aboutText text-2xl">
           <p>
-            Stu Finn is a <Emph>software developer</Emph> from Ontario, Canada.
-            He has been building websites since 2017 and in 2019 he started{" "}
+            I am a <Emph>software developer</Emph> from Ontario, Canada. I've
+            been building websites since 2017 and in 2019, I started{" "}
             <a
               href="https://www.borealisweb.ca"
               target="_blank"
@@ -63,18 +64,22 @@ const About = ({ data }) => {
             </a>
             , a small design and development agency that is focused on creating
             entirely custom and easy-to-use websites.{" "}
-            <a href="#more">Learn More</a>
+            <a href="#more">
+              <p className="mt-2 text-right">
+                More About Stu <AiOutlineArrowDown className="inline" />
+              </p>
+            </a>
           </p>
-          <H2 className="animate-pulse">Hire Stu!</H2>
+          <H2 className="animate-pulse">*Hire Me!*</H2>
           <p>
-            Stu is currently <Emph>looking for work</Emph> as a part time
+            I am currently <Emph>looking for work</Emph> as a part time
             front-end developer for a remote-first company.
           </p>
           <div>
             <H2>Get In Touch</H2>
 
             <p>
-              For more info about Stu, check him out on{" "}
+              For more info check me out on{" "}
               <LogoLink url="https://github.com/stufinn/">
                 <LogoContainer>
                   <AiFillGithub />
@@ -97,7 +102,7 @@ const About = ({ data }) => {
               </LogoLink>
               , or just{" "}
               <Emph>
-                <Link to="/contact">drop him a line</Link>
+                <Link to="/contact">drop me a line</Link>
               </Emph>
               !
             </p>
@@ -118,33 +123,33 @@ const About = ({ data }) => {
             </p> */}
           </div>
           <h1 className="cursor-pointer text-center mt-20" id="more">
-            Even More About Stu
+            Even More Info
           </h1>
           <div>
             <H2>The Stack</H2>
             <p>
-              These days, web technologies of choice for Stu are{" "}
+              These days, my web technologies of choice are{" "}
               <Emph> JavaScript, React, Gatsby, Node.js</Emph>, and of course{" "}
               <Emph>HTML</Emph> and <Emph>CSS</Emph>. But it doesn’t stop there;
-              as a self-taught developer, Stu is always looking for ways to
-              strengthen and extend his skillset.{" "}
+              as a self-taught developer, I am always looking for ways to
+              strengthen and extend my skillset.{" "}
             </p>
             <H2>The Before (Dev) Times</H2>
             <p>
-              Before to becoming a full time developer in 2018, Stu was
+              Before to becoming a full time developer in 2018, I was
               alternately a project manager, a museum curator, an environmental
-              scientist, and a barista among other things. He has an
+              scientist, and a barista among other things. I hold an
               undergraduate degree in Biology (minor in International
               Development), a masters degree in Environmental Studies, and a
-              certification in Project Management. He knows lots of stuff!
+              certification in Project Management. I lots of stuff!
             </p>
             <H2>The Fun Stuff</H2>
 
             <p>
-              Stu is a quirky mix between tech-kid and outdoorsman. When he’s
-              not building things at his computer he loves spending time with
-              his family on the lake near his home in remote Northern Ontario,
-              or in the nearby woods camping, biking, or skiing.
+              I am a quirky mix between tech-kid and outdoorsman. When I'm not
+              building things at my computer I love spending time with my family
+              on the lake near my home in remote Northern Ontario, or in the
+              nearby woods camping, biking, or skiing.
             </p>
             <div className="ml-5">
               <h3 className="text-white font-thin text-4xl mt-5 mb-3">
@@ -152,18 +157,18 @@ const About = ({ data }) => {
               </h3>
               <ul className="mb-5 grid gap-y-2">
                 <Li>
-                  Stu loves to travel ✈️. To date he has visited 21 countries
-                  and is eager to do more.
+                  I love to travel ✈️. To date I've visited more than 20
+                  countries and I am eager to do more.
                 </Li>
                 <Li>
-                  He is an enthusiastic sailor. To date he has competed in a
-                  300-mile and a 90-mile sailing race on Lake Ontario. He has
-                  won a grand total of zero races, but he always has a blast
+                  I am an enthusiastic sailor. To date I have competed in a
+                  300-mile and a 90-mile sailing race on Lake Ontario. I won a
+                  grand total of zero of those races, but I always have a blast
                   doing them.
                 </Li>
                 <Li>
-                  On warm summer evenings you can find Stu sitting around a camp
-                  fire with friends, often with a guitar in his lap.
+                  On warm summer evenings you can usually find me sitting around
+                  a camp fire with friends, often with a guitar in my lap.
                 </Li>
               </ul>
             </div>
