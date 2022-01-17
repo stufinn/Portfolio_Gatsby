7beb6c9deb1db1ad399c134e73b2af8b3293f001
Stu Finn <stu@borealisweb.ca>
2022-01-17 16:46:03 -0600

    Update success message

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,7 @@ const Success = () => {
     <div className="">
       <div className="grid justify-items-center text-4xl text-center mt-10 ">
         <p className="">Thank you - your message has been submitted!</p>
-        <p className="mt-5 ">Stu will be in touch with you soon.</p>
+        <p className="mt-5 ">I'll be in touch with you soon.</p>
         <ButtonLink text="Home Page" className="text-2xl mt-10" />
       </div>
     </div>
